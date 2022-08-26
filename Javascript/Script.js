efd427fbf37c57dbd7aeffaa26fee7d8032f9a75
NetFitt <mohamedzerouali203@gmail.com>
2022-08-26 22:21:12 +0100

    2nd patch

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -3,6 +3,13 @@ let btn = document.querySelector(".selected");
 let select = document.querySelector(".select");
 
 btn.addEventListener("click",()=>{
-   select.classList.toggle() 
+   if(select.classList.contains("open")){
+        select.classList.add("close")
+        select.classList.remove("open")
+
+   }else{
+        select.classList.add("open")
+        select.classList.remove("close")
+   }
 
 });

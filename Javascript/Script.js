b5e3e79fad47aaa254506074399a6d6ab803afb7
NetFitt <mohamedzerouali203@gmail.com>
2022-08-27 18:59:43 +0100

    still need a lot of work thou

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,7 +1,14 @@
 
+class real {
+     
+
+
+
+
+}
+
 let btn = document.querySelector(".selected");
 let select = document.querySelector(".select");
-
 btn.addEventListener("click",()=>{
    if(select.classList.contains("open")){
         select.classList.add("close")
@@ -12,4 +19,83 @@ btn.addEventListener("click",()=>{
         select.classList.remove("close")
    }
 
+});
+
+//THE MAGIC 
+
+// btn is the selcted 
+const region = document.querySelectorAll(".select__menu_option");
+
+const search = document.querySelector(".search")
+
+const API = new XMLHttpRequest()
+API.open("GET" , "https://restcountries.com/v3.1/all")
+
+API.send()
+
+API.addEventListener("load",()=>{
+     const data = JSON.parse(API.responseText)
+     const grid = document.querySelector(".grid")
+
+     
+     
+     data.forEach(element => {
+          let img = element.flags.png;
+          let name = element.name.common;
+          let population = element.population;
+          let region = element.region;
+          let capital = element.capital;
+          // the items 
+          let country = document.createElement("div");
+          country.classList.add("grid_item")
+          country.innerHTML = `
+          
+               <img src="${img}" alt="${img}">
+               <h3 class="grid_item_title">${name}</h3>
+               <div class="grid_item_text">
+                    <h5>Population:</h5><p> ${population}</p>
+               </div class="grid_item_text">
+               <div class="grid_item_text">
+                    <h5>Region:</h5><p> ${region}</p>
+               </div>
+               <div class="grid_item_text">
+                    <h5>Capital:</h5><p> ${capital}</p>
+               </div>
+          
+          `;
+          grid.appendChild(country);
+
+          search.addEventListener("keyup",()=>{
+               let search_val = search.value
+               let patren = /[A-Z]/g;
+               // data.forEach((ele)=>{
+
+               // })
+               if(search_val === element.name.common){
+                    country.innerHTML = `
+                                        <img src="${img}" alt="${img}">
+                                        <h3 class="grid_item_title">${name}</h3>
+                                        <div class="grid_item_text">
+                                             <h5>Population:</h5><p> ${population}</p>
+                                        </div class="grid_item_text">
+                                        <div class="grid_item_text">
+                                             <h5>Region:</h5><p> ${region}</p>
+                                        </div>
+                                        <div class="grid_item_text">
+                                             <h5>Capital:</h5><p> ${capital}</p>
+                                        </div>
+                                        `;
+                    grid.innerHTML="";
+                    grid.appendChild(country);
+               }
+
+          })
+     });
+     
+     // console.log(Object.keys(data[0]))
+     // console.log(data[0].name)
+     // console.log(data[0].maps)
+     // console.log(data[0].region)
+     // console.log(data[0].flags)
+     
 });
